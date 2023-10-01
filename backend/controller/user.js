47bded0227f3f76309b7d458eac10dec12262806
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2023-10-02 00:12:08 +0500

    node mailer updated

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -48,6 +48,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
         email: user.email,
         subject: "Activate your account",
         message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+      
       });
       res.status(201).json({
         success: true,
