61029e68d11164812add115ab779ddd47d5d051f
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2023-10-28 02:12:26 +0500

    added pwa

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import Store from "./redux/store";
+import PWAPrompt from 'react-ios-pwa-prompt'
 
 ReactDOM.render(
   <Provider store={Store}>
-    <App />
+    <App /><PWAPrompt copyTitle="AutoEssentials "/>
   </Provider>,
   document.getElementById("root")
 );
